19dc54adacb136576d8c6e17c37c131943e23784
anderdam <anderdam@gmail.com>
2023-08-24 00:23:00 -0300

    Initial commit

---
+++ src/main/java/com/anderdam/project/realtimedataprocessdashboard/RealTimeDataProcessDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.anderdam.project.realtimedataprocessdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeDataProcessDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeDataProcessDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anderdam/project/realtimedataprocessdashboard/RealTimeDataProcessDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anderdam.project.realtimedataprocessdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeDataProcessDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
